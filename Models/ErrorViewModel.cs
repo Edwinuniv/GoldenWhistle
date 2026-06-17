e6c4068f54b1a9a3137c3040458bdfe09d49974c
Your Name <your-email@example.com>
2026-06-17 22:23:44 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoldenWhistle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
